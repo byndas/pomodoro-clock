b870831146c74514cc5d871115d8890afbe3560b
marc byndas <byndasma@gmail.com>
2017-04-26 19:33:09 -0400

    first commit

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use('/css', express.static(process.cwd() + '/css'));
 app.use('/js', express.static(process.cwd() + '/js'));
 app.use(favicon(path.join(__dirname,'favicon','favicon.ico')));
+app.use(audio(path.join(__dirname,'audio','koto.mp3')));
+app.use(audio(path.join(__dirname,'audio','shamisen.wav')));
 
 app.get('/', function(req, res){
 			res.sendFile(process.cwd() + '/main.html');
